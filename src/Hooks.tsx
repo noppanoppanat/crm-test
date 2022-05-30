ca4abb123dcb6fcf06e6375aca6fa8ff40a13e24
noppanoppanat <noppanatb62@nu.ac.th>
2022-05-30 22:33:20 +0700

    add Hook page

---
+++ src/Hooks.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css';
+
+function Hooks() {
+    return (
+        <div className="App">
+            <a className="App-header">
+                This is Hooks test page
+            </a>
+        </div>
+    );
+}
+
+export default Hooks;
